573760c0b1967d9e7f0b5cfc01b006641bedf009
alitiger1381 <alitigercr71381@gmail.com>
2024-03-18 21:28:04 +0330

    implement RepositoryModule

---
+++ app/src/main/java/com/alidev/cryptoalert/di/RepositoryModule.kt
@@ -0,0 +1,32 @@
+package com.alidev.cryptoalert.di
+
+import com.alidev.cryptoalert.data.repository.candle.CryptoCandlesRepository
+import com.alidev.cryptoalert.data.repository.candle.CryptoCandlesRepositoryDefault
+import com.alidev.cryptoalert.data.repository.candle.IndicatorRepository
+import com.alidev.cryptoalert.data.repository.candle.IndicatorRepositoryDefault
+import com.alidev.cryptoalert.data.repository.stats.CryptoMarketRepository
+import com.alidev.cryptoalert.data.repository.stats.CryptoMarketRepositoryDefault
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface RepositoryModule {
+
+    @Binds
+    fun bindCryptoMarketRepository(
+        repository: CryptoMarketRepositoryDefault
+    ): CryptoMarketRepository
+
+    @Binds
+    fun bindIndicatorRepository(
+        repository: IndicatorRepositoryDefault
+    ): IndicatorRepository
+
+    @Binds
+    fun bindCryptoCandlesRepository(
+        repository: CryptoCandlesRepositoryDefault
+    ): CryptoCandlesRepository
+}
