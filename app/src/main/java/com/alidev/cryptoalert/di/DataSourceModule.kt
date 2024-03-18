4ae3bc9ce2a0bedd4836209f39a95deef1a9cd5a
alitiger1381 <alitigercr71381@gmail.com>
2024-03-18 21:27:43 +0330

    implement DataSourceModule

---
+++ app/src/main/java/com/alidev/cryptoalert/di/DataSourceModule.kt
@@ -0,0 +1,32 @@
+package com.alidev.cryptoalert.di
+
+import com.alidev.cryptoalert.data.datasource.candle.CryptoCandlesDataSource
+import com.alidev.cryptoalert.data.datasource.candle.CryptoCandlesDataSourceDefault
+import com.alidev.cryptoalert.data.datasource.candle.IndicatorDataSource
+import com.alidev.cryptoalert.data.datasource.candle.IndicatorDataSourceDefault
+import com.alidev.cryptoalert.data.datasource.stats.CryptoMarketDataSource
+import com.alidev.cryptoalert.data.datasource.stats.CryptoMarketDataSourceDefault
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface DataSourceModule {
+
+    @Binds
+    fun bindCryptoMarketDataSource(
+        dataSource: CryptoMarketDataSourceDefault
+    ): CryptoMarketDataSource
+
+    @Binds
+    fun bindIndicatorDataSource(
+        dataSource: IndicatorDataSourceDefault
+    ): IndicatorDataSource
+
+    @Binds
+    fun bindCryptoCandlesDataSource(
+        dataSource: CryptoCandlesDataSourceDefault
+    ): CryptoCandlesDataSource
+}
