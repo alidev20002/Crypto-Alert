b4ea75ae13ab26d18027a42b9a52ef1521651e20
alitiger1381 <alitigercr71381@gmail.com>
2024-03-27 22:23:10 +0430

    handle conditions in crypto service

--- app/src/main/java/com/alidev/cryptoalert/service/CryptoAlertService.kt
+++ app/src/main/java/com/alidev/cryptoalert/service/CryptoAlertService.kt
@@ -1,10 +1,10 @@
 package com.alidev.cryptoalert.service
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
-import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.IBinder
@@ -13,10 +13,25 @@ import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import com.alidev.cryptoalert.MainActivity
 import com.alidev.cryptoalert.R
+import com.alidev.cryptoalert.data.repository.condition.ConditionRepository
+import com.alidev.cryptoalert.data.repository.stats.CryptoMarketRepository
+import com.alidev.cryptoalert.ui.model.Condition
+import com.alidev.cryptoalert.ui.model.CryptoCondition
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class CryptoAlertService : Service() {
+
+    @Inject
+    lateinit var cryptoMarketRepository: CryptoMarketRepository
+
+    @Inject
+    lateinit var conditionRepository: ConditionRepository
 
     private var isServiceStarted = false
 
@@ -27,28 +42,33 @@ class CryptoAlertService : Service() {
     override fun onCreate() {
         super.onCreate()
 
+        val notification = createNotification()
+
+        startForeground(NOTIFICATION_ID, notification)
+    }
+
+    private fun createNotification(): Notification {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
                 "crypto_stats_channel",
                 "Crypto Alert Notification",
                 NotificationManager.IMPORTANCE_HIGH
             )
-            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
         }
 
-        val pendingIntent: PendingIntent = Intent(this, MainActivity::class.java).let { notificationIntent ->
-            PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
-        }
+        val pendingIntent: PendingIntent =
+            Intent(this, MainActivity::class.java).let { notificationIntent ->
+                PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
+            }
 
-        val notification = NotificationCompat.Builder(this, "crypto_stats_channel")
+        return NotificationCompat.Builder(this, "crypto_stats_channel")
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentTitle("Crypto Alert")
             .setContentText("Price Checking...")
             .setContentIntent(pendingIntent)
             .build()
-
-        startForeground(1, notification)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -60,7 +80,6 @@ class CryptoAlertService : Service() {
 
             STOP -> {
                 stopServiceJob()
-                return START_NOT_STICKY
             }
         }
 
@@ -71,12 +90,56 @@ class CryptoAlertService : Service() {
         if (isServiceStarted) return
 
         isServiceStarted = true
-        // service job...
+
+        val conditions = conditionRepository.readConditionsSync().toMutableList()
+        val sourceCurrencies = conditions.joinToString(separator = ",") { it.crypto.name }
+        val destinationCurrency = "rls"
+
+        GlobalScope.launch(Dispatchers.IO) {
+            while (isServiceStarted) {
+                val market = cryptoMarketRepository.getStats(sourceCurrencies, destinationCurrency)
+                val stats = market.cryptoStats
+
+                val okConditions = mutableListOf<CryptoCondition>()
+
+                conditions.forEach { cryptoCondition ->
+                   val price = stats["${cryptoCondition.crypto.name}-$destinationCurrency"]?.latest
+                   price?.let {
+                       if (isConditionOk(price.toDouble(), cryptoCondition)) {
+                           okConditions.add(cryptoCondition)
+                           // create a notification with sound and vibration
+                       }
+                   }
+                }
+
+                conditions.removeAll(okConditions)
+
+                conditionRepository.writeConditionsSync(conditions)
+
+                if (conditions.isEmpty()) {
+                    stopServiceJob()
+                    return@launch
+                }
+
+                delay(DELAY_BETWEEN_REQUEST_MILLIS)
+            }
+        }
+    }
+
+    private fun isConditionOk(price: Double, crypto: CryptoCondition): Boolean {
+        return when(crypto.condition) {
+            Condition.INCREASE -> {
+                price >= crypto.expectedPrice
+            }
+
+            Condition.DECREASE -> {
+                price <= crypto.expectedPrice
+            }
+        }
     }
 
     private fun stopServiceJob() {
         try {
-            stopForeground(STOP_FOREGROUND_REMOVE)
             stopSelf()
         }catch (e: Exception) {
             Log.i("alitest", "stopServiceJob: ${e.message}")
@@ -86,6 +149,9 @@ class CryptoAlertService : Service() {
     }
 
     companion object {
+
+        private const val NOTIFICATION_ID = 8112560
+        private const val DELAY_BETWEEN_REQUEST_MILLIS = 15000L
 
         const val START = "START"
         const val STOP = "STOP"
