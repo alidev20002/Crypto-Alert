2bdc5f8b6e1cb0feb0620d942019b6868a3da855
alitiger1381 <alitigercr71381@gmail.com>
2024-03-27 22:23:53 +0430

    test service code in main activity

--- app/src/main/java/com/alidev/cryptoalert/MainActivity.kt
+++ app/src/main/java/com/alidev/cryptoalert/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.alidev.cryptoalert
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.alidev.cryptoalert.service.CryptoAlertService
 import com.alidev.cryptoalert.ui.theme.CryptoAlertTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,7 +29,33 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+
+                        Button(
+                            onClick = {
+                                Intent(applicationContext, CryptoAlertService::class.java).also {
+                                    it.action = CryptoAlertService.START
+                                    startForegroundService(it)
+                                }
+                            }
+                        ) {
+                            Text(text = "Start Service")
+                        }
+
+                        Button(
+                            onClick = {
+                                Intent(applicationContext, CryptoAlertService::class.java).also {
+                                    it.action = CryptoAlertService.STOP
+                                    startForegroundService(it)
+                                }
+                            }
+                        ) {
+                            Text(text = "Stop Service")
+                        }
+                    }
                 }
             }
         }
