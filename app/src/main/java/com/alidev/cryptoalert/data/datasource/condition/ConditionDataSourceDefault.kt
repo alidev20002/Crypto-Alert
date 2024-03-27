924ae49d2855d952a2b5214ae2971aa9febae3c4
alitiger1381 <alitigercr71381@gmail.com>
2024-03-27 22:20:15 +0430

    implement ConditionDataSource

---
+++ app/src/main/java/com/alidev/cryptoalert/data/datasource/condition/ConditionDataSourceDefault.kt
@@ -0,0 +1,26 @@
+package com.alidev.cryptoalert.data.datasource.condition
+
+import com.alidev.cryptoalert.data.datastore.ConditionDataStore
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ConditionDataSourceDefault @Inject constructor(
+    private val conditionDataStore: ConditionDataStore
+) : ConditionDataSource {
+
+    override fun readConditions(): Flow<String?> {
+        return conditionDataStore.readConditions()
+    }
+
+    override suspend fun writeConditions(value: String) {
+        conditionDataStore.writeConditions(value)
+    }
+
+    override fun readConditionsSync(): String? {
+        return conditionDataStore.readConditionsSync()
+    }
+
+    override fun writeConditionsSync(value: String) {
+        conditionDataStore.writeConditionsSync(value)
+    }
+}
