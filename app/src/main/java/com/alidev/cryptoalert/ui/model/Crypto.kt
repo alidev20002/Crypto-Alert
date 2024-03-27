e20c91759b894a3a1f16422b4709274c27271c00
alitiger1381 <alitigercr71381@gmail.com>
2024-03-27 22:18:37 +0430

    implement Crypto model

---
+++ app/src/main/java/com/alidev/cryptoalert/ui/model/Crypto.kt
@@ -0,0 +1,28 @@
+package com.alidev.cryptoalert.ui.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Crypto(
+    @SerialName("name")
+    val name: String,
+
+    @SerialName("lowPrice")
+    val lowPrice: Double = 0.0,
+
+    @SerialName("highPrice")
+    val highPrice: Double = 0.0,
+
+    @SerialName("openPrice")
+    val openPrice: Double = 0.0,
+
+    @SerialName("latestPrice")
+    val latestPrice: Double = 0.0,
+
+    @SerialName("change")
+    val change: Double,
+
+    @SerialName("icon")
+    val icon: Int
+)
