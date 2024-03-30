c43ec1904e8c19162840dd5ad43ea9830488fe35
alitiger1381 <alitigercr71381@gmail.com>
2024-03-30 16:04:16 +0430

    implement MarketState

---
+++ app/src/main/java/com/alidev/cryptoalert/ui/viewmodel/stats/MarketState.kt
@@ -0,0 +1,13 @@
+package com.alidev.cryptoalert.ui.viewmodel.stats
+
+import com.alidev.cryptoalert.data.api.CryptoMarket
+import com.alidev.cryptoalert.ui.model.CryptoCondition
+
+interface MarketState
+
+data class CryptoMarketState(
+    val cryptoConditions: List<CryptoCondition>,
+    val cryptoMarket: CryptoMarket
+) : MarketState
+
+object EmptyMarketState : MarketState
