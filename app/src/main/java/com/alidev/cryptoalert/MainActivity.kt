c1486a23515ce394bdeb7d253c779a66207aebfa
alitiger1381 <alitigercr71381@gmail.com>
2024-04-01 21:44:54 +0430

    testing dst currency in ui

--- app/src/main/java/com/alidev/cryptoalert/MainActivity.kt
+++ app/src/main/java/com/alidev/cryptoalert/MainActivity.kt
@@ -6,14 +6,22 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -25,6 +33,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.alidev.cryptoalert.service.CryptoAlertService
@@ -58,6 +67,10 @@ class MainActivity : ComponentActivity() {
 
                     var conditionId by rememberSaveable {
                         mutableStateOf(1.0)
+                    }
+
+                    var isRial by rememberSaveable {
+                        mutableStateOf(true)
                     }
 
                     Column(
@@ -94,7 +107,11 @@ class MainActivity : ComponentActivity() {
                                 val conditions = state.cryptoConditions
                                 isEnabled = conditions.isNotEmpty()
 
-                                LazyColumn {
+                                LazyColumn(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(250.dp)
+                                ) {
                                     items(conditions) {
                                         Row(
                                             modifier = Modifier
@@ -142,6 +159,56 @@ class MainActivity : ComponentActivity() {
                                     }
                                 ) {
                                     Text(text = "Add new Condition")
+                                }
+                                
+                                Spacer(modifier = Modifier.height(20.dp))
+                                
+                                val stats = state.cryptoMarket.cryptoStats.entries.toList()
+                                isRial = state.dstCurrency == "rls"
+
+                                Row {
+                                    Button(
+                                        colors = ButtonDefaults.buttonColors(
+                                            containerColor = if(isRial) Color.Cyan else Color.Red
+                                        ),
+                                        onClick = {
+                                            viewModel.saveDstCurrency("usdt")
+                                            viewModel.syncCryptoStats("usdt")
+                                        }
+                                    ) {
+                                        Text(text = "USDT")
+                                    }
+
+                                    Button(
+                                        colors = ButtonDefaults.buttonColors(
+                                            containerColor = if(!isRial) Color.Cyan else Color.Red
+                                        ),
+                                        onClick = {
+                                            viewModel.saveDstCurrency("rls")
+                                            viewModel.syncCryptoStats("rls")
+                                        }
+                                    ) {
+                                        Text(text = "RLS")
+                                    }
+                                }
+                                LazyRow(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(250.dp),
+                                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                                ) {
+                                    
+                                    items(stats) {
+                                        Column(
+                                            modifier = Modifier
+                                                .background(Color.Gray, RoundedCornerShape(15.dp))
+                                                .padding(8.dp),
+                                            horizontalAlignment = Alignment.CenterHorizontally
+                                        ) {
+                                            Text(text = it.value.latest)
+                                            Text(text = it.key)
+                                        }
+                                    }
                                 }
                             }
                         }
