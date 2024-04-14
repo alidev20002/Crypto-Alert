8c948df5f3224a5918a254bd4ca7a10813896d12
alitiger1381 <alitigercr71381@gmail.com>
2024-04-14 21:18:21 +0430

    implement crypto stats screen

---
+++ app/src/main/java/com/alidev/cryptoalert/ui/screen/CryptoStatsScreen.kt
@@ -0,0 +1,390 @@
+package com.alidev.cryptoalert.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.alidev.cryptoalert.R
+import com.alidev.cryptoalert.ui.model.Condition
+import com.alidev.cryptoalert.ui.model.Crypto
+import com.alidev.cryptoalert.ui.viewmodel.stats.CryptoMarketViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CryptoStatsScreen(
+    cryptos: List<Crypto>,
+    onAddClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+    var selectedCrypto by remember {
+        mutableStateOf(cryptos[0])
+    }
+    
+    var expectedPrice by remember {
+        mutableStateOf(cryptos[0].latestPrice)
+    }
+
+    var condition by remember {
+        mutableStateOf(Condition.INCREASE)
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFF1E1E1E))
+    ) {
+
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+
+            item {
+                Spacer(modifier = Modifier.width(0.dp))
+            }
+
+            items(cryptos) {
+
+                Column(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(16.dp))
+                        .background(Color(0xFF9142FF))
+                        .width(120.dp)
+                        .padding(10.dp)
+                ) {
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+
+                        Text(
+                            text = it.shortName,
+                            color = Color(0xFFFFFFFF)
+                        )
+
+                        Text(
+                            text = it.change,
+                            color = Color(0xFFAFAEAE)
+                        )
+                    }
+
+                    Text(
+                        modifier = Modifier
+                            .padding(top = 5.dp),
+                        text = it.latestPrice,
+                        color = Color(0xFFFFFFFF),
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight(500)
+                    )
+
+                    Spacer(modifier = Modifier.height(20.dp))
+
+                    Image(
+                        painter = painterResource(id = it.icon),
+                        contentDescription = "",
+                        contentScale = ContentScale.FillBounds
+                    )
+                }
+            }
+
+            item {
+                Spacer(modifier = Modifier.width(0.dp))
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+                .shadow(4.dp, RoundedCornerShape(12.dp))
+                .background(Color(0xFF272727), RoundedCornerShape(12.dp))
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF9142FF)
+                ),
+                onClick = { expanded = true },
+            ) {
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+
+                    Image(
+                        painter = painterResource(id = selectedCrypto.icon),
+                        contentDescription = "",
+                        contentScale = ContentScale.FillBounds
+                    )
+
+                    Text(
+                        text = selectedCrypto.shortName,
+                        color = Color(0xFFFFFFFF)
+                    )
+                }
+            }
+
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp)
+                    .background(Color(0x00000000))
+                    .border(2.dp, Color(0xFF9142FF), RoundedCornerShape(8.dp))
+                    .padding(horizontal = 8.dp),
+                value = expectedPrice,
+                onValueChange = {
+                    expectedPrice = it
+                },
+                singleLine = true,
+                maxLines = 1,
+                shape = RoundedCornerShape(8.dp),
+                textStyle = TextStyle(
+                    textAlign = TextAlign.Center,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight(500)
+                ),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.Unspecified,
+                    unfocusedBorderColor = Color.Unspecified,
+                    textColor = Color(0xFFFFFFFF)
+                ),
+                leadingIcon = {
+                    Icon(
+                        modifier = Modifier
+                            .size(36.dp)
+                            .clip(CircleShape)
+                            .clickable(
+                                interactionSource = MutableInteractionSource(),
+                                indication = rememberRipple()
+                            ) {
+                                expectedPrice = "${expectedPrice.toDouble() - 100}"
+                            }
+                            .padding(8.dp),
+                        painter = painterResource(id = R.drawable.minus_icon),
+                        contentDescription = ""
+                    )
+                },
+                trailingIcon = {
+                    Icon(
+                        modifier = Modifier
+                            .size(36.dp)
+                            .clip(CircleShape)
+                            .clickable(
+                                interactionSource = MutableInteractionSource(),
+                                indication = rememberRipple()
+                            ) {
+                                expectedPrice = "${expectedPrice.toDouble() + 100}"
+                            }
+                            .padding(8.dp),
+                        painter = painterResource(id = R.drawable.plus_icon),
+                        contentDescription = ""
+                    )
+                }
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+
+                val increaseBackgroundColor = if (condition == Condition.INCREASE)
+                    Color(0xFF9142FF)
+                else
+                    Color(0xFFAFAEAE)
+
+                val decreaseBackgroundColor = if (condition == Condition.INCREASE)
+                    Color(0xFFAFAEAE)
+                else
+                    Color(0xFF9142FF)
+
+                Button(
+                    shape = RoundedCornerShape(16.dp, 0.dp, 0.dp, 16.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = increaseBackgroundColor
+                    ),
+                    onClick = {
+                        condition = Condition.INCREASE
+                    }
+                ) {
+
+                    Text(
+                        text = "Increase",
+                        color = Color(0xFFFFFFFF),
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight(700)
+                    )
+                }
+
+                Button(
+                    shape = RoundedCornerShape(0.dp, 16.dp, 16.dp, 0.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = decreaseBackgroundColor
+                    ),
+                    onClick = {
+                        condition = Condition.DECREASE
+                    }
+                ) {
+                    Text(
+                        text = "Decrease",
+                        color = Color(0xFFFFFFFF),
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight(700)
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF9142FF)
+                ),
+                onClick = onAddClick
+            ) {
+                Text(
+                    text = "Add Condition",
+                    color = Color(0xFFFFFFFF),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700)
+                )
+            }
+        }
+
+    }
+
+    if (expanded) {
+        Dialog(
+            onDismissRequest = { expanded = false }
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color(0xFF272727), RoundedCornerShape(16.dp)),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(
+                    modifier = Modifier
+                        .padding(vertical = 8.dp),
+                    text = "Select Crypto",
+                    color = Color(0xFFFFFFFF),
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight(700)
+                )
+
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                ) {
+
+                    items(cryptos) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable(
+                                    interactionSource = MutableInteractionSource(),
+                                    indication = rememberRipple()
+                                ) {
+                                    selectedCrypto = it
+                                    expectedPrice = it.latestPrice
+                                    expanded = false
+                                }
+                                .padding(8.dp),
+                            horizontalArrangement = Arrangement.spacedBy(10.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+
+                            Image(
+                                painter = painterResource(id = it.icon),
+                                contentDescription = "",
+                                contentScale = ContentScale.FillBounds
+                            )
+
+                            Text(
+                                text = it.shortName,
+                                color = Color(0xFFFFFFFF)
+                            )
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+@Preview(showBackground = true)
+private fun CryptoStatsScreenPreview() {
+    CryptoStatsScreen(
+        cryptos = CryptoMarketViewModel.getListOfAvailableCryptos(),
+        onAddClick = {}
+    )
+}
